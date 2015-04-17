ea727528eb88b15d80d2b441e44a87720d58ecad
Madahin <nico99999@hotmail.fr>
2015-04-17 18:19:42 +0000

    Begining of the commenting of table_diff_div

--- TP1/table_diff_div.m
+++ TP1/table_diff_div.m
@@ -1,13 +1,21 @@
-function [ tab ] = table_diff_div( x, y )
+function tab = table_diff_div( x, y )
+% TABLE_DIFF_DIV Génère la table des différence divisé
+% x : Vecteur des abscisses des point de support
+% y : Vecteur des ordonnées des point de support
 
 sizeX = numel(x);
 sizeY = numel(y);
 
+
+% On test la taille des vecteur
 if (sizeX ~= sizeY)
-    % echoue
+    % Dans ce cas, on devrai échoué
 end
 
+
+% On crée une table dont les deux premier colonne sont nos vecteur d'entrés
 tab = [x', y', zeros(sizeX, sizeX-1)];
+% Écart de la pyramide des différence divisé
 gap = 1;
 
 for i = 1:sizeX-1
