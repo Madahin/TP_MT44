9db07321068c5bdaf253d6891581a4e3294b1d60
Madahin <nico99999@hotmail.fr>
2015-04-17 17:27:04 +0000

    commenting ExecTime1.m

--- TP1/ExecTime1.m
+++ TP1/ExecTime1.m
@@ -1,15 +1,33 @@
+% On définit les données d'un polynôme
+% d'interpolation vu en TD
 n = 3;
 a = [1.5709 0.0006 0.00012];
 c = [1 4 6];
 t = 3.5;
 
-tic;
-evaluation1(n, a, c, t);
-elapsed1 = toc
 
-tic;
-evaluation(n, a, c, t);
-elapsed2 = toc;
+% Pour avoir des données correctes, on test
+% la fonction 1000 fois et on regarde la moyenne
+% du temps d'execution
+sum1 = 0;
+for z=0:1000
+    tic;
+    evaluation1(n, a, c, t);
+    sum1 = sum1 + toc; 
+end
+elapsed1 = sum1/1000;
 
-disp(elapsed1)
-disp(elapsed2)
+
+% Même chose que pour evaluation1
+sum2 = 0;
+for z=0:1000
+    tic;
+    evaluation(n, a, c, t);
+    sum2 = sum2 + toc;
+end
+elapsed2 = sum2 / 1000;
+
+
+% On affiche les valeurs
+disp(elapsed1);
+disp(elapsed2);
