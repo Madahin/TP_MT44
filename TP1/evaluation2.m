adbf4bc8bc31d2393e6adb2140bd495793bcc035
Madahin <nico99999@hotmail.fr>
2015-04-17 18:01:20 +0000

    Commenting evaluation2.m

--- TP1/evaluation2.m
+++ TP1/evaluation2.m
@@ -1,4 +1,5 @@
-function [ vars ] = evaluation2( n, a, c, t )
+function vars = evaluation2( n, a, c, t )
+% EVALUATION2 version matricelle de évaluation(n, a, c, t)
 
 PNumber = numel(t);
 vars = zeros(1, PNumber);
@@ -6,8 +7,5 @@ vars = zeros(1, PNumber);
 for i = 1:PNumber
     vars(i) = evaluation(n, a, c, t(i));
 end
-
-
-
 end
 
