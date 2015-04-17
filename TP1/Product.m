10df43e8aff4fcfa6536cf0f96743d696df07c53
Madahin <nico99999@hotmail.fr>
2015-04-17 17:36:39 +0000

    Correction

--- TP1/Product.m
+++ TP1/Product.m
@@ -2,7 +2,7 @@ function prod = Product( i, x, c )
 %PRODUCT Calcule les polynôme de Newton
 %   i : Degré du polynôme
 %   x : Valeur pour laquelle évaluer la fonction
-%   c : Point de pivot
+%   c : Point de support
 
 prod = 1;
 for j = 1:i
