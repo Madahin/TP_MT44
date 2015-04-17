259800e5a84af14291c6f4cc597d60a6eb374ecf
Madahin <nico99999@hotmail.fr>
2015-04-17 17:33:25 +0000

    Commenting Product.m

--- TP1/Product.m
+++ TP1/Product.m
@@ -1,6 +1,8 @@
-function [ prod ] = Product( i, x, c )
-%PRODUCT Summary of this function goes here
-%   Detailed explanation goes here
+function prod = Product( i, x, c )
+%PRODUCT Calcule les polynôme de Newton
+%   i : Degré du polynôme
+%   x : Valeur pour laquelle évaluer la fonction
+%   c : Point de pivot
 
 prod = 1;
 for j = 1:i
