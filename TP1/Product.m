b149b81e8e3238681eab1cdeaed17f20fecfb10c
Madahin <nico99999@hotmail.fr>
2015-04-17 17:38:52 +0000

    Adding a comment for the return value

--- TP1/Product.m
+++ TP1/Product.m
@@ -1,8 +1,9 @@
 function prod = Product( i, x, c )
 %PRODUCT Calcule les polynôme de Newton
-%   i : Degré du polynôme
-%   x : Valeur pour laquelle évaluer la fonction
-%   c : Point de support
+%    i : Degré du polynôme
+%    x : Valeur pour laquelle évaluer la fonction
+%    c : Point de support
+% prod : le polynôme de Newton
 
 prod = 1;
 for j = 1:i
