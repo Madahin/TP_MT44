2f2d4dafec57a9bca903dafb4e61f4fad2d614bb
Madahin <nico99999@hotmail.fr>
2015-05-26 23:30:54 +0000

    adding a third dimension and changing the function to use 3 array of value instead of 1 array of point

--- TP2/CastelJau.m
+++ TP2/CastelJau.m
@@ -1,22 +1,33 @@
-function [ Pn ] = casteljau( Points, t )
+function [ Pnx, Pny, Pnz ] = CastelJau( Px, Py, Pz, t )
 %CASTELJAU Summary of this function goes here
 %   Detailed explanation goes here
 
-n = numel(Points)/2 -1;
-Pn = zeros(n+1, numel(Points));
-Px = zeros(1, numel(t));
-Py = zeros(1, numel(t));
+Pnx = zeros(1, numel(t));
+Pny = zeros(1, numel(t));
+Pnz = zeros(1, numel(t));
+
+n = numel(Px) -1;
+
+Ptmp = zeros(3, n+1, n+1);
+
 for j=1:numel(t)
-    for p=1:numel(Points)
-        Pn(1, p) = Points(p);
+    for i=1:n+1
+        Ptmp(1, i, 1) = Px(i);
+        Ptmp(2, i, 1) = Py(i);
+        Ptmp(3, i, 1) = Pz(i);
     end
-    for r=1:n
-        for i=1:numel(Points)-r-1 -(r-1)
-            Pn(r+1, i) = (1 - t(j)) * Pn(r, i) + t(j) * Pn(r, i+2);
+    
+    for r=0:n-1
+        for i=r:n-1
+            Ptmp(1, 1 + i + 1, 1 + r + 1) = (1 - t(j)) * Ptmp(1, 1 + i, 1 + r) + t(j) * Ptmp(1, 1 + i + 1, 1 + r);
+            Ptmp(2, 1 + i + 1, 1 + r + 1) = (1 - t(j)) * Ptmp(2, 1 + i, 1 + r) + t(j) * Ptmp(2, 1 + i + 1, 1 + r);
+            Ptmp(3, 1 + i + 1, 1 + r + 1) = (1 - t(j)) * Ptmp(3, 1 + i, 1 + r) + t(j) * Ptmp(3, 1 + i + 1, 1 + r);
         end
     end
-    Px(j) = Pn(n+1, 1);
-    Py(j) = Pn(n+1, 2);
+    
+    Pnx(j) = Ptmp(1, n+1, n+1);
+    Pny(j) = Ptmp(2, n+1, n+1);
+    Pnz(j) = Ptmp(3, n+1, n+1);
 end
 
 end
