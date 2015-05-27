00ec875c8552e37a7fed6e6d6080f3950246d4c9
Tony Duong <tonyduong@live.fr>
2015-05-27 14:05:53 +0200

    partie 2 termine

--- TP2/genere_bik.m
+++ TP2/genere_bik.m
@@ -1,26 +1,26 @@
-function [ Bik ] = genere_bik( noeud, k )
+function [ Bik ] = genere_bik( noeud, k, trace)
 %GENERE_BIK Summary of this function goes here
 %   Detailed explanation goes here
-    if (k > 4) 
-        error('k must be lower than 4.');
+    if (k+1 > 4) 
+        error('k+1 must be lower than 4.');
     end
     
     syms x;
     syms Bik;
     
-    size = length(noeud);
-    Bik = zeros(size-1,size-1,k);
+    Length = length(noeud);
+    Bik = zeros(Length-1,Length-1,k+1);
     Bik = sym(Bik);
-    for ktemp=1:k
+    for ktemp=1:k+1
         if ktemp == 1
             % Bi,0
-            for i=1:size-1
+            for i=1:Length-1
                 if (noeud(i)<=noeud(i) && noeud(i)<noeud(i+1))
                     Bik(i,i,1) = 1;
                 end
             end
         else
-           for i=1:size-ktemp
+           for i=1:Length-ktemp
                if (noeud(i+ktemp-1) ~= noeud(i))
                     s1 = (x-noeud(i)).*Bik(i,:,ktemp-1)./(noeud(i+ktemp-1)-noeud(i));
                else
@@ -35,6 +35,25 @@ function [ Bik ] = genere_bik( noeud, k )
                
                Bik(i,:, ktemp) = s1 + s2; 
            end
+        end
+    end
+    
+    if trace == 1
+        d = size(Bik);
+        figure
+        for i=1:d(1)
+            
+            subplot(d(1), 1, i)
+            for j=1:d(2) 
+                
+                X = [noeud(j) noeud(j+1)];
+                if (Bik(i,j,k+1) ~= 0)
+                    sn = matlabFunction(Bik(i,j,k+1));                
+                    fplot(sn,X); hold on;
+                end
+            end
+            xlim([noeud(1) noeud(Length)]);
+            title(sprintf('B-Splines de degré %i pour i = %i', k+1-1, i-1));
         end
     end
 end
