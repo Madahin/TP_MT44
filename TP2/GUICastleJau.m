94a4f30a07490b2d28dc740b14b848c11ec97481
Madahin <nico99999@hotmail.fr>
2015-06-05 11:37:21 +0000

    fixing the gui

--- TP2/GUICastleJau.m
+++ TP2/GUICastleJau.m
@@ -1,14 +1,15 @@
  function GUICastleJau
-    % Create a figure and axes
+    % Cree la figure
     f = figure('Visible','off');
-    set(f, 'Position', [0, 0, 1024, 768]);
-    ax = axes('Units','pixels');  
+    set(f, 'Position', [0, 0, 1024, 768]); 
     
-    n = 3;
+    % On definit nos point de support
+    n = 15;
     Px = rand(1, n);
     Py = rand(1, n);
     Pz = rand(1, n);
     
+    % On calcule notre courbe de bezier avec CastleJau
     [Pnx, Pny, Pnz] = CastelJau(Px, Py, Pz, 0:0.001:1);
 
     %  On trace une droite passant par nos point originaux
@@ -17,51 +18,57 @@
     % On trace notre courbe de bezier.
     plot3(Pnx, Pny, Pnz, 'color', 'r');
     
-   % Create slider
+   % On cree un slider qui controle la valeur de t
     sld = uicontrol('Style', 'slider',...
         'Min',0,'Max',1,'Value',0,...
         'Position', [20 20 400 20],...
         'Callback', @surfzlim); 
     
-    % Make figure visble after adding all components
+    % On rend la figgure fisible une fois que tout les element on ete
+    % ajoute
     set(f, 'Visible', 'on');
-    % This code uses dot notation to set properties. 
-    % Dot notation runs in R2014b and later.
-    % For R2014a and earlier: set(f,'Visible','on');
     
+    % Ne connaissant pas la valeur NULL sous matlab, nous utilisont 0.
+    % Ces variable contienne les different segment et le point qui se
+    % deplace le long de la courbe
     fp = 0;
-    fnp = zeros(1, n);
+    fnp = zeros(n, n-1);
 
     function surfzlim(source,callbackdata)
-        
+        % Si on a deja dessiner le point, on l'efface
         if fp ~= 0
             delete(fp);
         end
         
+        % Si on a deja dessiner les segment, on les effaces
         for i=1:n
-            if fnp(i) ~= 0
-                delete(fnp(i));
+            for j=1:n-1
+                if fnp(i, j) ~= 0
+                    delete(fnp(i, j));
+                end
             end
         end
         
-        
+        % On recupere la valeur du slider
         val = get(source,'Value');
          
+        % On calcule la position du point par rapport a notre slider et on
+        % recupere les calcule intermediaire de l'algorithme de CastleJau
         [nPx, nPy, nPz, tmp] = CastelJau(Px, Py, Pz, val);
          
+        % On trace notre point
         fp = plot3(nPx, nPy, nPz, 'C*');
         
         for i=3:n
-            lx = [tmp(1, i-1, i-1), tmp(1, i, i-1)];
-            ly = [tmp(2, i-1, i-1), tmp(2, i, i-1)];
-            lz = [tmp(3, i-1, i-1), tmp(3, i, i-1)];
-            fnp(i) = line(lx, ly, lz, 'color', 'g');
-        end
-        
-        
-         
-        disp(tmp);
-        %disp(tmp(1, 2, 2));
-         
+            for j=i-1:n-1
+                % On recupere dans les calcule de CastleJau les coordonner
+                % des different segments
+                lx = [tmp(1, j, i-1), tmp(1, j+1, i-1)];
+                ly = [tmp(2, j, i-1), tmp(2, j+1, i-1)];
+                lz = [tmp(3, j, i-1), tmp(3, j+1, i-1)];
+                % On trace les segments
+                fnp(i, j) = line(lx, ly, lz, 'color', 'g');
+            end
+        end         
     end
 end
