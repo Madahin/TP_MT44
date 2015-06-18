bf67ec4d7a0aec0791a1171e3ac6766236448cee
Madahin <nico99999@hotmail.fr>
2015-06-18 16:32:42 +0000

    fixing gauss + add error test

--- TP3/integration_gaussienne.m
+++ TP3/integration_gaussienne.m
@@ -1,38 +1,53 @@
 function [ In ] = integration_gaussienne( type, nb_points, r )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    r = matlabFunction(r);
+%INTEGRATION_GAUSSIENNE Calcule l'integration de la fonction symbolique
+%regularise r avec nb_points points en utilisant la methode type,
+%pour les valeurs :
+%0 -> Legendre
+%1 -> Tchebyschev
+
     In = 0;
+    % On transoforme la fonction symbolique en fonction de matlab
+    r = matlabFunction(r);
     switch type
     case 0
-        % Intégration Legendre
+        % Integration Legendre
+        % On recupere les polynomes de Legendre pour le nombres de points
+        % donne
         poly_legendre = polynome_legendre(nb_points);
+        % On alloue un peu de memoire pour contenir les racine des
+        % polynomes
         zeros_legendre = zeros(nb_points-1, nb_points-1);
         for i=2:nb_points
-            zeros_legendre(:, i-1) = [solve(poly_legendre(i)); zeros(nb_points-i, 1)];
+            % On calcule les zeros des polynomes de Legendre
+            a = double(roots(sym2poly((poly_legendre(i)))));
+            %disp(poly_legendre(i));
+            %disp(a);
+            zeros_legendre(:, i-1) = [a; zeros(nb_points-i, 1)];
         end
+        % On s'interesse a la transpose de la matrice
         zeros_legendre = zeros_legendre';
+        % On calcules les coefficients de Legendre
         coefficients_legendre = coeff_legendre(poly_legendre, zeros_legendre);
         
         for i=1:nb_points-1
-            xi = double(zeros_legendre(nb_points-1, i));    
+            % On recupere la ieme racine des polynome de Legendre
+            xi = double(zeros_legendre(nb_points-1, i));
+            % On 
             In = In + coefficients_legendre(nb_points-1, i)*r(xi);
         end
     case 1
-        % Intégration Tchebyschev
-        syms x;
+        % Integration Tchebyschev
         zeros_tchebyschev = zeros(nb_points-1, nb_points-1);
         poly_tchebyschev = polynome_tchebyschev(nb_points);
         for i=2:nb_points
-            zeros_tchebyschev(:, i-1) = [solve(poly_tchebyschev(i)); zeros(nb_points-i, 1)];
+            zeros_tchebyschev(:, i-1) = [roots(sym2poly(poly_tchebyschev(i))); zeros(nb_points-i, 1)];
         end
         zeros_tchebyschev = zeros_tchebyschev';
         coefficients_tchebyschev = coeff_tchebyschev(zeros_tchebyschev);
-        coefficients_tchebyschev = coefficients_tchebyschev.*sqrt(1-x^2);
         
         for i=1:nb_points-1
             xi = double(zeros_tchebyschev(nb_points-1, i));    
-            In = In + coefficients_tchebyschev(i)*r(xi);
+            In = In + coefficients_tchebyschev(nb_points-1)*r(xi) * sqrt(1-xi^2);
         end
     end
 
---
+++ TP3/test_error_gauss.m
@@ -0,0 +1,47 @@
+clear;
+clc;
+
+% definition de quelques constantes
+% couleurs des courbes
+color = ['r', 'b'];
+
+% nombre de tests
+N = 15;
+
+
+
+% Il nous faut une variable symbolique pour calculer l'integral 'juste'
+% dans un interval donne
+syms x;
+
+% definition des fonctions a integrer
+f = [sin(x);1/(1+x^2);exp(x)];
+titles = ['sin(x)   ';'1/(1+x^2)';'exp(x)   '];
+
+% recupere le nombre de fonction a integrer disponnible
+nbFunc = size(f);
+nbFunc = nbFunc(1);
+
+for n=1:nbFunc
+    % On calcule l'integrale 'juste'
+    integ_reel = int(f(n, :), x, -1, 1);
+    for i=0:1
+        % On alloue de la memoire pour contenir nos resultat
+        integ_calc = zeros(1, N);
+        for j=1:N
+            % On calcule la valeur absolue de la difference entre nos
+            % integrale numerique et l'integrale 'juste' pour la nieme
+            % fonction, avec la ieme methode, avec j nombre de points
+            integ_calc(j) = abs(integ_reel - integration_gaussienne(i, j, f(n, :)));
+        end
+        % On dessine les courbe representant l'erreur en fonction du nombre
+        % de points
+        subplot(nbFunc, 1, n);
+        plot(integ_calc, 'color', color(i+1));
+        title(titles(n, :));
+        % Les warning n'on aucune importances
+        legend('gauss/legendre', 'gauss/tchebyschev');
+        %title(f(n, :));
+        hold on;
+    end
+end
